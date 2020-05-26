d56e4ca1678dd2ceed6ea101ca306b057be506c5
Gabriel Falcones <gafalcon@espol.edu.ec>
2020-05-25 23:17:12 -0400

    create write tab component

---
+++ src/components/writeTab/WriteTab.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import MCEditor from '../editor/MCEditor'
+import {Container, Row, Col, Form} from 'react-bootstrap'
+import ScaffoldSelect from '../scaffold/ScaffoldSelect'
+
+import './WriteTab.css';
+class WriteTab extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.onScaffoldSelected = this.onScaffoldSelected.bind(this);
+    }
+
+    onScaffoldSelected(tagCreator, initialText){
+        console.log(tagCreator, initialText)
+        this.props.onChange({scaffold: {tagCreator, initialText}})
+    }
+
+    render() {
+        const {note, onChange, onEditorSetup} = this.props;
+        return (
+            <Container className='write-container p-0'>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="note-title">
+                                <Form.Control type="text"
+                                              size="sm"
+                                              placeholder="Enter title"
+                                              value={note.title}
+                                              onChange={(val) => {onChange({title: val.target.value})}}/>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={3} className='pr-0'>
+                            <ScaffoldSelect initVal={0} onScaffoldSelected={this.onScaffoldSelected}/>
+                        </Col>
+                        <Col md={9}>
+                            <MCEditor value={note.content}
+                                      onEditorSetup={onEditorSetup}
+                                      onEditorChange={(content, editor) => onChange({content})}/>
+
+                        </Col>
+                    </Row>
+            </Container>
+        )
+    }
+}
+
+export default WriteTab;
