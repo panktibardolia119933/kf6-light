cf48b49282cbefe47bb70ea3a767c7beba28790e
Gabriel Falcones <gafalcon@espol.edu.ec>
2020-05-25 23:12:01 -0400

    create editor component

---
+++ src/components/editor/MCEditor.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+/* import './editor.css' */
+
+class MCEditor extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Editor
+                    value={this.props.content}
+                    apiKey="arg05azt52qbujpnf831szuswhmyhoqute0q48btk5bqigoj"
+                    init={{
+                        setup: (editor) => {this.props.onEditorSetup(editor)},
+                        content_css: './editor.css',
+                        height: 500,
+                        menubar: false,
+                        plugins: [
+                            'advlist autolink lists link image charmap print preview anchor',
+                            'searchreplace visualblocks code fullscreen',
+                            'insertdatetime media table paste code help'
+                        ],
+                        external_plugins: {
+                            'drawingTool': '/drawing-tool/plugin.min.js'
+                        },
+                        toolbar: 'styleselect | bold italic underline strikethrough | forecolor backcolor bullist numlist | link code | ltr rtl | charmap | drawingTool'
+                    }}
+                    onEditorChange={this.props.onEditorChange}
+                />
+            </div>
+        );
+    }
+}
+
+export default MCEditor;
